c6533009a3ea0585185d0ce53851072e69559010
benjaminkariuki <benhaminleyian6@gmail.com>
2026-04-24 04:46:46 +0300

    downgrade axios to version 1 .3.3

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,29 +1,14 @@
 // src/api/axios.js
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-    withXSRFToken: true,
-});
-
-// Manually attach XSRF-TOKEN because withXSRFToken
-// only works for same-origin by default
-api.interceptors.request.use((config) => {
-    const token = document.cookie
-        .split('; ')
-        .find(row => row.startsWith('XSRF-TOKEN='))
-        ?.split('=')[1];
-
-    if (token) {
-        config.headers['X-XSRF-TOKEN'] = decodeURIComponent(token);
-    }
-
-    return config;
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+  // remove withXSRFToken - this option didn't exist in 1.3.3
 });
 
 export default api;
